54e26657724914e782744493e4f14416967e0324
cloudteamid <cloudb8.it@gmail.com>
2019-04-14 14:15:18 +0700

    Informasi Update1

--- web/Initio/informasi.php
+++ web/Initio/informasi.php
@@ -43,13 +43,8 @@
 			<div class="navbar-collapse collapse">
 				
 				<ul class="nav navbar-nav">
-<<<<<<< HEAD:web/Initio/about.html
-					<li class="active"><a href="index.html">Beranda</a></li>
-					<li><a href="about.html">About</a></li>
-=======
 					<li><a href="index.php">Beranda</a></li>
 					<li class="active"><a href="informasi.php">About</a></li>
->>>>>>> 4930608af24a81f72e62e9fb7d5c73977ee32b82:web/Initio/informasi.php
 					<li><a href="list-peserta.php">Peserta</a></li>
 					<li><a href="pendaftaran.php">Pendaftaran</a></li>
 			
@@ -64,7 +59,7 @@
 		<h2 class="section-title2"><span>Informasi Umum Acara</span></h2>
 		<div class="row section topspace">
 			<div class="col-md-12">
-				<p class="lead text-center">Agar peserta dapat mengkompetisikan kemampuannya, peserta juga akan memiliki niat dan pandangan
+				<p class="lead text-center">Acara bertujuan agar peserta dapat mengkompetisikan kemampuannya, peserta juga akan memiliki niat dan pandangan
 				untuk menjadi generasi akuntansi yang gemilang. Peserta juga akan menunjukkan dan
 				mengkompetisikan kemampuannya dibidang akuntansi baik dalam teori maupun praktik, peserta juga
 				nantinya dapat menganalisis akuntansi di era kini, meningkatkan kemampuan intelektual dan
@@ -75,39 +70,18 @@
 		<hr class="hr-style1">
 		
 		<!-- OPSI 1 -->
-		<h2 class="section-title3"><span class="glyphicon glyphicon-cog" style="font-size:20px; color:#eac338"></span> Peraturan Perlombaan CC</h2>
-		<hr class="hr-style2"></hr>
-		<!-- <h2 class="section-title4"><span><tr><td>1</td><td> Cerdas cermat terdiri dari 3 orang dalam 1 regu.</td></tr></span></h2>
-		<h2 class="section-title4"><span><tr><td>2</td><td> 1 sekolah maksimal mengirimkan 2 tim.</td></tr></span></h2>
-		<h2 class="section-title4"><span><tr><td>3</td><td> Peserta berada di ruangan cerdas cermat 5 menit sebelum sesi Cerdas Cermat dimulai.</td></tr></span></h2>
-		<h2 class="section-title4"><span><tr><td>4</td><td> Jika peserta datang terlambat maka Regu yang bersangkutan akan didiskualifikasi.</td></tr></span></h2>
-		<h2 class="section-title4"><span><tr><td>5</td><td> Peserta dilarang menggunakan alat bantu hitung dalam bentuk apapun.</td></tr></span></h2>
-		<h2 class="section-title4"><span><tr><td>6</td><td> Regu yang termasuk ke babak semifinal 8 regu, kemudian regu yang masuk final ada 4 regu diambil dari poin tertinggi dari tiap sesi.</td></tr></span></h2>
-		 -->
-		<table>
-			<tr><th><span class="glyphicon glyphicon-pencil" style="font-size:12px; color:#292929"></span></th><th>Cerdas cermat terdiri dari 3 orang dalam 1 regu.</th></tr>
-			<tr><th><span class="glyphicon glyphicon-pencil" style="font-size:12px; color:#292929"></span></th><th>1 sekolah maksimal mengirimkan 2 tim.</th></tr>
-			<tr><th><span class="glyphicon glyphicon-pencil" style="font-size:12px; color:#292929"></span></th><th>Peserta berada di ruangan cerdas cermat 5 menit sebelum sesi Cerdas Cermat dimulai.</th></tr>
-			<tr><th><span class="glyphicon glyphicon-pencil" style="font-size:12px; color:#292929"></span></th><th>Jika peserta datang terlambat maka Regu yang bersangkutan akan didiskualifikasi.</th></tr>
-			<tr><th><span class="glyphicon glyphicon-pencil" style="font-size:12px; color:#292929"></span></th><th>Peserta dilarang menggunakan alat bantu hitung dalam bentuk apapun.</th></tr>
-			<tr><th><span class="glyphicon glyphicon-pencil" style="font-size:12px; color:#292929"></span></th><th>Regu yang termasuk ke babak semifinal 8 regu, kemudian regu yang masuk final ada 4 regu diambil dari poin tertinggi dari tiap sesi.</th></tr>
+		<table class="informasi-header">
+		<tr><th class="informasi-header-isi">Gelombang Pendaftaran Acara IHSAO 2019</th></tr>
 		</table>
-		<!-- OPSI 2 -->
-		<h2 class="section-title3"><span class="glyphicon glyphicon-list-alt" style="font-size:20px; color:#eac338"></span> CC Soal Wajib (Kisi-Kisi)</h2>
-		<hr class="hr-style3"></hr>
-		<table>
-			<tr><th><span class="glyphicon glyphicon-ok" style="font-size:12px; color:#292929"></span></th><th>Untuk soal wajib semua regu akan mendapatkan 5 soal wajib dalam 1 sesi.</th></tr>
-			<tr><th><span class="glyphicon glyphicon-ok" style="font-size:12px; color:#292929"></span></th><th>Soal wajib untuk masing-masing regu dipilih secara acak oleh peserta yang akan dibantu panitia.</th></tr>
-			<tr><th><span class="glyphicon glyphicon-ok" style="font-size:12px; color:#292929"></span></th><th>Saat soal wajib telah selesai dibacakan oleh pembaca soal, setiap regu yang diberi kesempatan akan diberikan waktu 1 menit untuk menjawab.</th></tr>
-			<tr><th><span class="glyphicon glyphicon-ok" style="font-size:12px; color:#292929"></span></th><th>Untuk soal wajib setelah 1 menit berlalu bel tanda waktu telah habis akan berbunyi dan juri akan memberikan waktu selama 3 detik, jika peserta tidak juga menjawab maka akan diberikan kesempatan pada regu yang lain.</th></tr>
-			<tr><th><span class="glyphicon glyphicon-ok" style="font-size:12px; color:#292929"></span></th><th>Apabila regu menekan bel sebelum soal selesai dibacakan oleh pembaca soal maka regu tersebut tidak diperbolehkan menjawab soal, dan soal akan diperebutkan oleh regu lain. A.2 Cerdas Cermat Soal Rebutan</th></tr>
-			<tr><th><span class="glyphicon glyphicon-ok" style="font-size:12px; color:#292929"></span></th><th>Untuk soal rebutan semua regu akan mendapatkan 10 soal rebutan dalam 1 sesi.</th></tr>
-			<tr><th><span class="glyphicon glyphicon-ok" style="font-size:12px; color:#292929"></span></th><th>Untuk soal rebutan regu lain bisa menekan bel setelah juri mengatakan “salah”.</th></tr>
-			<tr><th><span class="glyphicon glyphicon-ok" style="font-size:12px; color:#292929"></span></th><th>Regu yang berhak menjawab setiap pertanyaan rebutan adalah regu yang ditunjuk oleh juri.</th></tr>
+		<table class="informasi-isi">
+		<tr><th class="informasi-isi-isi"><b>Gelombang I</b><br>1 Juni 2019 - 17 Juni 2019</th><th class="informasi-isi-isi"><b>Gelombang II</b><br>20 Juni 2019 - 1 Juli 2019</th></tr>
+		<tr><th class="informasi-isi-isi"><b>Cerdas Cermat</b> <br>Rp.300.000 (Penginapan)<br>Rp.280.000 (Tanpa Penginapan)</th><th class="informasi-isi-isi"><b>Cerdas Cermat </b><br>Rp.320.000 (Penginapan)<br>Rp.300.000 (Tanpa Penginapan)</th></tr>
+		<tr><th class="informasi-isi-isi"><b>Olimpiade </b><br>Rp.300.000 (Penginapan)<br>Rp.280.000 (Tanpa Penginapan)</th><th class="informasi-isi-isi"><b>Olimpiade </b><br>Rp.320.000 (Penginapan)<br>Rp.300.000 (Tanpa Penginapan)</th></tr>
+		<tr><th class="informasi-isi-isi"><b>MYOB </b><br>Rp.300.000 (Penginapan)<br>Rp.280.000 (Tanpa Penginapan)</th><th class="informasi-isi-isi"><b>MYOB </b><br>Rp.320.000 (Penginapan)<br>Rp.300.000 (Tanpa Penginapan)</th></tr>
+		<tr><th class="informasi-isi-isi"><b>Debat </b><br>Rp.270.000 (Penginapan)<br>Rp.250.000 (Tanpa Penginapan)</th><th class="informasi-isi-isi"><b>Cerdas Cermat </b><br>Rp.300.000 (Penginapan)<br>Rp.280.000 (Tanpa Penginapan)</th></tr>
+		<tr><th class="informasi-isi-isi"><b>Karya Ilmiah </b><br>Rp.250.000 (Tanpa Penginapan)</th><th class="informasi-isi-isi"><b>Karya Ilmiah </b><br>Rp.250.000 (Tanpa Penginapan)</th></tr>
 		</table>
-		
 		<br><br><br>
-		
 	</div>
 
 </main>
