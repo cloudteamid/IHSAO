3c56261fac6eda6bc1a2f3242bc1935f59468c4f
cloudteamid <cloudb8.it@gmail.com>
2019-04-16 12:48:37 +0700

    new header

--- web/Initio/index.php
+++ web/Initio/index.php
@@ -24,22 +24,17 @@
 <body class="home">
 
 <header id="header">
-	<div id="head" class="parallax" parallax-speed="2">
-
+	<div id="head">
 		<h1 id="logo" class="text-center">
-			<img class="img-circle" src="assets/images/guy.jpg" alt="">
-			<span class="title">IHSAO 2019</span>
-			<span class="tagline">Compete Your Skill to be a Brilliant <b>Accounting</b> Generation<br></span> 
+			<br class="spasi">
 		</h1>
 	</div>
-
 	<nav class="navbar navbar-default navbar-sticky">
 		<div class="container-fluid">
-			
+
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
 			</div>
-			
 			<div class="navbar-collapse collapse">
 				
 				<ul class="nav navbar-nav">
@@ -48,10 +43,10 @@
 					<li><a href="list-peserta.php">Peserta</a></li>
 					<li><a href="pendaftaran.php">Pendaftaran</a></li>
 				</ul>
-			
 			</div><!--/.nav-collapse -->			
 		</div>	
 	</nav>
+	
 </header>
 
 <main id="main">
